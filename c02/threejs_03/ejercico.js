37d4b23f0a58ecdc4d915938cd0ab3b61e51425f
6000510 <141768739+6000510@users.noreply.github.com>
2023-09-26 14:10:14 -0500

    Add files via upload
    agregue camara y renderizador

---
+++ c02/threejs_03/ejercico.js
@@ -0,0 +1,21 @@
+
+function init() {
+    // Crear una escena
+    scene = new THREE.Scene();
+
+    // Crear una cámara
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Crear un renderizador
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('canvas-container').appendChild(renderer.domElement);
+
+    // Crear una geometría personalizada (cono)
+    createCustomCone();
+
+    // Agregar iluminación
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1);
+    scene.add(light);
