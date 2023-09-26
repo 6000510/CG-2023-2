1ecc9d84f05307c19b24fda820584abf19763bf6
6000510 <141768739+6000510@users.noreply.github.com>
2023-09-26 14:36:48 -0500

    Add files via upload
    añadia la textura y el html

--- c02/threejs_03/ejercico.js
+++ c02/threejs_03/ejercico.js
@@ -1,3 +1,5 @@
+// Variables globales
+let scene, camera, renderer, cone, controls;
 
 function init() {
     // Crear una escena
@@ -19,3 +21,56 @@ function init() {
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(1, 1, 1);
     scene.add(light);
+
+    // Iniciar controles para el usuario
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+function createCustomCone() {
+    // Crear una geometría personalizada (cono)
+    const geometry = new THREE.BufferGeometry();
+    const radius = 2;
+    const height = 5;
+    const radialSegments = 20;
+
+    const positions = [];
+    const colors = [];
+
+    for (let i = 0; i <= radialSegments; i++) {
+        const theta = (i / radialSegments) * Math.PI * 2;
+        const x = radius * Math.cos(theta);
+        const z = radius * Math.sin(theta);
+        const y = (i / radialSegments) * height;
+
+        positions.push(x, y, z);
+        colors.push(1, 0, 0); // Color del vértice (rojo)
+    }
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+    // Crear un material
+    const material = new THREE.PointsMaterial({ vertexColors: true });
+
+    // Crear un objeto 3D (Puntos) utilizando la geometría y el material
+    cone = new THREE.Points(geometry, material);
+    scene.add(cone);
+}
+
+function startInteractiveScene() {
+    init();
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotación interactiva del cono
+    if (controls) {
+        controls.update();
+    }
+
+    renderer.render(scene, camera);
+}
+
+window.onload = startInteractiveScene;
